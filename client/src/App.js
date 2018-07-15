f999d378c683eb965d0d8640558874378aed8fe7
unknown <kylestauch99@gmail.com>
2018-07-14 23:08:26 -0500

    Setup redux, store, and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
+
+// Redux store
+import store from './store';
 
 import Landing from './components/Landing/Landing';
 
@@ -7,7 +13,13 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<Landing />
+			<Provider store={store}>
+				<BrowserRouter>
+					<Switch>
+						<Route exact path="/" component={Landing} />
+					</Switch>
+				</BrowserRouter>
+			</Provider>
 		);
 	}
 }
