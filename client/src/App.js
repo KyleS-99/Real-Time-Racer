26fa1c3c5b5af684c389b1faae866e8ad05ed515
unknown <kylestauch99@gmail.com>
2018-07-15 22:19:45 -0500

    Added routes, and setup Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,23 @@ import Loadable from 'react-loadable';
 // Redux store
 import store from './store';
 
+import Navbar from './components/Navbar/Navbar';
 import Landing from './components/Landing/Landing';
 
 import './App.css';
+import 'normalize.css';
 
 class App extends Component {
 	render() {
 		return (
 			<Provider store={store}>
 				<BrowserRouter>
-					<Switch>
-						<Route exact path="/" component={Landing} />
-					</Switch>
+					<div>
+						<Navbar />
+						<Switch>
+							<Route exact path="/" component={Landing} />
+						</Switch>
+					</div>
 				</BrowserRouter>
 			</Provider>
 		);
