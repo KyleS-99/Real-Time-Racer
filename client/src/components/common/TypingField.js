a1e978f0a59383794261a97ffbf7c612389e0a89
unknown <kylestauch99@gmail.com>
2018-07-21 23:23:45 -0500

    Create TypingField component

---
+++ client/src/components/common/TypingField.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+class TypingField extends Component {
+    state = {  }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default TypingField;
