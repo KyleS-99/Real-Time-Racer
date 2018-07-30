608af7ab0e1c7c63b3bec951b908f9d5a9782b5d
unknown <kylestauch99@gmail.com>
2018-07-30 13:47:16 -0500

    Created ProgressBar component

---
+++ client/src/components/common/ProgressBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+class ProgressBar extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>ProgressBar Component</div>
+        );
+    }
+}
+
+export default ProgressBar;
