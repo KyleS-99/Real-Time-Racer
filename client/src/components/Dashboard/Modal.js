a1ca8e044ce8312e91319ea2ea3e9fa397df8de7
unknown <kylestauch99@gmail.com>
2018-09-25 18:02:22 -0500

    Added Modal component

---
+++ client/src/components/Dashboard/Modal.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const ModalContainer = styled.div`
+
+`;
+
+const Modal = (props) => (
+	
+);
+
+export default Modal;
