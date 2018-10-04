0955f0cae82c921cc1ba824b47fde5172d7cc4ce
unknown <kylestauch99@gmail.com>
2018-10-03 21:23:34 -0500

    Created ModalTitle component

--- client/src/components/Dashboard/Modal/CustomText.js
+++ client/src/components/Dashboard/Modal/CustomText.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import BorderLine from '../../styled/BorderLine';
-
-const ModalTitle = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 18px;
-`;
-
-const ModalTitleParagraph = styled.p`
-	color: #fff;
-	text-transform: uppercase;
-	font-weight: 300;
-	font-size: 15px;
-	margin: 0;
-`;
-
-const Img = styled.img`
-	cursor: pointer;
-	width: 40px;
-`;
+import ModalTitle from './ModalTitle';
 
 class CustomText extends Component {
 	render() {
 		return (
-			<ModalTitle>
-				<div>
-					<ModalTitleParagraph>Custom Text</ModalTitleParagraph>
-					<BorderLine />
-				</div>
-				<Img src={window.location.origin + '/img/x.png'} alt="Close" />
-			</ModalTitle>
+			<ModalTitle text="Custom Text" />
 		); 
 	}
 }
---
+++ client/src/components/Dashboard/Modal/ModalTitle.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import BorderLine from '../../styled/BorderLine';
+
+const ModalTitleContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 18px;
+`;
+
+const ModalTitleParagraph = styled.p`
+	color: #fff;
+	text-transform: uppercase;
+	font-weight: 300;
+	font-size: 15px;
+	margin: 0;
+`;
+
+const Img = styled.img`
+	cursor: pointer;
+	width: 40px;
+`;
+
+const ModalTitle = (props) => (
+    <ModalTitleContainer>
+        <div>
+            <ModalTitleParagraph>{props.text}</ModalTitleParagraph>
+            <BorderLine />
+        </div>
+        <Img src={window.location.origin + '/img/x.png'} alt="Close" />
+    </ModalTitleContainer>
+);
+
+export default ModalTitle;
