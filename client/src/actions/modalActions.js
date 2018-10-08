8da2ce107f1b3dfaf2597897d4f4612dab40464c
unknown <kylestauch99@gmail.com>
2018-10-08 06:45:25 -0500

    Finished modal actions

--- client/src/actions/modalActions.js
+++ client/src/actions/modalActions.js
@@ -1,2 +1,16 @@
-import { CLOSE_MODAL, TOGGLE_MODAL } from './types';
+import { CLOSE_MODAL, TOGGLE_MODAL, SET_CUSTOM_TEXT } from './types';
 
+export const closeModal = () => ({
+    type: CLOSE_MODAL
+});
+
+export const toggleModal = () => ({
+    type: TOGGLE_MODAL
+});
+
+export const setCustomText = ({ text }) => ({
+    type: SET_CUSTOM_TEXT,
+    payload: {
+        text
+    }
+});
