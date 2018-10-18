6b47bf24556900ec88f78e298d7b71101edf197c
unknown <kylestauch99@gmail.com>
2018-10-18 03:29:57 -0500

    Created Multiplayer component.

---
+++ client/src/components/Dashboard/Modal/Multiplayer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Multiplayer extends Component {
+    state = {  }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Multiplayer;
