25448feab80097c337b98f7a1d55f1f2359859bb
unknown <kylestauch99@gmail.com>
2018-11-12 20:25:20 -0600

    Added match and history to props of Invite component

--- client/src/components/Invite/InviteContainer.js
+++ client/src/components/Invite/InviteContainer.js
@@ -2,8 +2,8 @@ import React from 'react';
 import SocketContextConsumer from '../common/SocketContextConsumer';
 import Invite from './Invite';
 
-const InviteContainer = () => (
-    <SocketContextConsumer render={(socket) => <Invite socket={socket} />} />
+const InviteContainer = (props) => (
+    <SocketContextConsumer render={(socket) => <Invite socket={socket} match={props.match} history={props.history} />} />
 );
 
 export default InviteContainer;
